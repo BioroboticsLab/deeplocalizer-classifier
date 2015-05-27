aedd26ee8963cf099f69883f914d753d6c6c18c0
berleon <github@leon-sixt.de>
2015-05-27 12:22:16 +0200

    [ManuellyLocalizer] test nextImage

--- test/TestManuellyLocalizer.cpp
+++ test/TestManuellyLocalizer.cpp
@@ -2,12 +2,15 @@
 #define CATCH_CONFIG_MAIN
 
 #include <boost/format.hpp>
+#include <boost/filesystem/operations.hpp>
 
 #include "catch.hpp"
 #include "ManuellyLocalizer.h"
 
 using namespace std;
+using namespace boost;
 using namespace deeplocalizer::tagger;
+
 
 TEST_CASE( "ManuellyLocalizer ", "[ManuellyLocalizer]" ) {
     vector<QString> image_paths = {QString("testdata/with_5_tags.jpeg")};
@@ -28,6 +31,40 @@ TEST_CASE( "ManuellyLocalizer ", "[ManuellyLocalizer]" ) {
         }
     }
 
+    SECTION( "gives you images" ) {
+        ManuellyLocalizer localizer(image_paths);
+        GIVEN( "one image with bees" ) {
+            THEN ( "it will give you only one image") {
+                boost::optional<Image &> opt_image = localizer.nextImage();
+                REQUIRE(opt_image.is_initialized());
+                Image &img = opt_image.get();
+                REQUIRE(img.filename == image_paths.at(0));
+                opt_image = localizer.nextImage();
+                REQUIRE(! opt_image.is_initialized());
+            }
+        }
+        GIVEN( "multiple images" ) {
+            THEN ( "it will give you multiple images") {
+                int n = 5;
+                std::vector<QString> n_paths;
+                cv::Mat img(128, 128, CV_8U, cv::Scalar(0));
+                for(int i = 0; i < n; i++) {
+                    filesystem::path path = filesystem::unique_path();
+                    path.replace_extension(".png");
+                    INFO(path.string());
+                    cv::imwrite(path.string(), img);
+                    n_paths.push_back(QString::fromStdString(path.string()));
+                }
+                localizer = ManuellyLocalizer(n_paths);
+                for(int i = 0; i < n; i++) {
+                    INFO("AT i: " << i);
+                    auto img = localizer.nextImage();
+                    REQUIRE(img.is_initialized());
+                }
+                REQUIRE(! localizer.nextImage().is_initialized());
+            }
+        }
+    }
     SECTION( "gives you tags" ) {
         ManuellyLocalizer localizer(image_paths);
         GIVEN( "an image with bees" ) {
