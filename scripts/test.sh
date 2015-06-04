6e5499d62b44461554b42f950f92d764815e1e20
berleon <github@leon-sixt.de>
2015-06-04 14:24:56 +0200

    helper script to compile & run tests

---
+++ scripts/test.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+set -e
+
+BUILD_DIR="build-test"
+mkdir -p $BUILD_DIR
+NCPUS=$(grep -c ^processor /proc/cpuinfo)
+(cd $BUILD_DIR;
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j $NCPUS
+ctest
+)
