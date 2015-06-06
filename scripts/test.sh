b785dfcb624b0425bf520e6915360afadca57c9f
berleon <github@leon-sixt.de>
2015-06-06 15:34:29 +0200

    run tests with clang and gcc

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,10 +2,20 @@
 
 set -e
 
-BUILD_DIR="build-test"
-mkdir -p $BUILD_DIR
-(cd $BUILD_DIR;
-cmake -DCMAKE_BUILD_TYPE=Debug ..
-make
-ctest
-)
+function build_and_test {
+    export CXX=$1
+    BUILD_DIR=$2
+    mkdir -p $BUILD_DIR
+    (cd $BUILD_DIR;
+    cmake -DCMAKE_BUILD_TYPE=Debug ..
+    make -j2
+    ctest
+    )
+}
+
+CLANG_BUILD_DIR="build-clang"
+GCC_BUILD_DIR="build-gcc"
+Clang="/usr/bin/clang++"
+GCC="/usr/bin/c++"
+build_and_test $Clang $CLANG_BUILD_DIR
+build_and_test $GCC $GCC_BUILD_DIR
