4ad146c77dcc66a4050635b62da6d992aea2101a
berleon <github@leon-sixt.de>
2015-06-07 23:38:56 +0200

    [add_border] generate pathfile

--- source/add_border.cpp
+++ source/add_border.cpp
@@ -8,6 +8,7 @@ using namespace deeplocalizer::tagger;
 using namespace std::chrono;
 namespace po = boost::program_options;
 namespace io = boost::filesystem;
+using boost::optional;
 
 po::options_description desc_option("Options");
 po::positional_options_description positional_opt;
@@ -17,7 +18,9 @@ time_point<system_clock> start_time;
 void setupOptions() {
     desc_option.add_options()
             ("help,h", "Print help messages")
-            ("output_dir,o", po::value<std::string>(), "Write images to this directory")
+            ("output-dir,o", po::value<std::string>(), "Write images to this directory")
+            ("output-pathfile", po::value<std::string>(),
+             "Write output_pathfile to this directory. Default is <output_dir>/images.txt")
             ("pathfile", po::value<std::vector<std::string>>(), "File with paths");
 
     positional_opt.add("pathfile", 1);
@@ -31,10 +34,24 @@ io::path addWb(io::path filename) {
     return output_path;
 }
 
-int run(const std::vector<ImageDesc> image_descs, io::path & output_dir) {
+void writeOutputPathfile(io::path pathfile, const std::vector<std::string> && output_paths) {
+    std::ofstream of(pathfile.string());
+    for(const auto & path : output_paths) {
+        of << path << '\n';
+    }
+    of << std::flush;
+
+    std::cout << std::endl;
+    std::cout << "Add border to " << output_paths.size() << " images. Saved new images paths to: " << std::endl;
+    std::cout << pathfile.string() << std::endl;
+}
+
+int run(const std::vector<ImageDesc> image_descs, io::path & output_dir,
+        optional<io::path> output_pathfile) {
     io::create_directories(output_dir);
     start_time = system_clock::now();
     printProgress(start_time, 0);
+    std::vector<std::string> output_paths;
     for (unsigned int i = 0; i < image_descs.size(); i++) {
         const ImageDesc & desc = image_descs.at(i);
         Image img(desc);
@@ -45,10 +62,14 @@ int run(const std::vector<ImageDesc> image_descs, io::path & output_dir) {
             std::cerr << "Fail to write image : " << output.string() << std::endl;
             return 1;
         }
+        output_paths.emplace_back(output.string());
         printProgress(start_time, static_cast<double>(i+1)/image_descs.size());
     }
+    writeOutputPathfile(output_pathfile.get_value_or(output_dir / "images.txt"),
+                        std::move(output_paths));
     return 0;
 }
+
 int main(int argc, char* argv[])
 {
     setupOptions();
@@ -62,12 +83,18 @@ int main(int argc, char* argv[])
         std::cout << desc_option << std::endl;
         return 0;
     }
-    if(vm.count("pathfile") && vm.count("output_dir")) {
+    if(vm.count("pathfile") && vm.count("output-dir")) {
         std::string pathfile =
                 vm.at("pathfile").as<std::vector<std::string>>().at(0);
         auto image_descs = ImageDesc::fromPathFile(pathfile);
-        auto output_dir = io::path(vm.at("output_dir").as<std::string>());
-        run(image_descs, output_dir);
+        auto output_dir = io::path(vm.at("output-dir").as<std::string>());
+
+        optional<io::path> output_pathfile;
+        if(vm.count("output-pathfile")) {
+            output_pathfile = boost::make_optional(
+                    io::path(vm.at("output-pathfile").as<std::string>()));
+        }
+        run(image_descs, output_dir, output_pathfile);
     } else {
         std::cout << "No pathfile or output_dir are given" << std::endl;
         std::cout << "Usage: add_border [options] pathfile.txt "<< std::endl;
