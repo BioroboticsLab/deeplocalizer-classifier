5a8957030e9c49d105e53211657459ec235dc73c
berleon <github@leon-sixt.de>
2015-07-21 13:46:37 +0200

    make classifier standalone repository

--- scripts/pick_n.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-N="$1"
-if [ "$N" == "" ]; then
-    echo "Usage: $0 N         where N is an integer."
-    echo "Picks randomly N lines from stdin"
-    exit 1;
-fi;
-cat <&0 | sort -R | head -n "$N"
